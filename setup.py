f09bd5ea1a49f3425eb15cf491a466b029eb05cb
raifpy <50047969+raifpy@users.noreply.github.com>
2020-06-28 23:07:07 +0300

    0.2

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md") as ld:
 
 setuptools.setup(
     name = "pyproperty",
-    version = "0.1",
+    version = "0.2",
     author = "raifpy",
-    author_email = "mail.raifpy@gmail.com",
+    author_email = "omerto12.45y@gmail.com",
     description="Python Explore Modules Func Tool *",
     long_description=long_description,
     url = "https://github.com/raifpy/pyproperty",
     packages=setuptools.find_packages(),
+    install_requires = ["colorama"],
     python_requires= ">=3.5",
     scripts = ["pyproperty/pyproperty"]
         )
